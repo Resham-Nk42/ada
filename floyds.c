d5b59fcccc2c208a62d69d2e218817a4a8d04a44
Resham-Nk42 <resham.cs24@bmsce.ac.in>
2025-05-07 12:24:54 +0530

    Add files via upload

---
+++ floyds.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#define INF 999
+#define MAX 10
+
+void floyd(int a[MAX][MAX], int n) {
+    int d[MAX][MAX];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            d[i][j] = a[i][j];
+        }
+    }
+
+    
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (d[i][k] + d[k][j] < d[i][j]) {
+                    d[i][j] = d[i][k] + d[k][j];
+                }
+            }
+        }
+    }
+
+   
+    printf("Shortest Distance Matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (d[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf("%7d", d[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
+
+    int a[MAX][MAX];
+
+    printf("Enter the cost adjacency matrix (use %d for INF):\n", INF);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    floyd(a, n);
+
+    return 0;
+}
